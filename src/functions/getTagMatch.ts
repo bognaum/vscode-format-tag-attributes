0a835900bd55e76cbc9641ba222d265cb27488f2
Bognaum <bognaum@gmail.com>
2022-06-21 08:49:20 +0300

    Splited by functions.

--- src/functions/getTagMatch.ts
+++ src/functions/getTagMatch.ts
@@ -5,6 +5,7 @@ for (const i in rawRE) {
 	// console.log(i, new RegExp(rawRE[i])); //Ёбаный тайпскрипт ругается
 }
 const 
+	styleRE      = new RegExp(rawRE.style, ""),
 	tagRE        = new RegExp(rawRE.tag , "y"),
 	attrRE       = new RegExp(rawRE.attr, ""),
 	attributesRE = new RegExp(rawRE.attr, "g" );
@@ -13,37 +14,14 @@ export default function getTagMatch (doc: vsc.TextDocument, pos: vsc.Position) {
 	const
 		offset = doc.offsetAt(pos),
 		text = doc.getText();
-	let 
-		i = offset, 
-		baseIndent = "", 
-		startOffset = 0,
-		endOffset = 0,
-		v = "", vv = "", a = 0, b = 0;
-	while (v = text[i]) {
-		if (v === "<") {
-			startOffset = i;
-			break;
-		}
-		i --;
-	}
-	while (vv = text[i]) {
-		if (vv === "\n") {
-			break;
-		}
-		i --;
-	}
-	i += 1;
-	while (vv = text[i]) {
-		if (![" ", "\t"].includes(vv)) {
-			break;
-		}
-		baseIndent += vv;
-		i ++;
-	}
-	if (v && text[a + 1] !== "/") {
+	const 
+		startOffset = getTagStartOffset(text, offset),
+		baseIndent  = getBaseIndent(text, startOffset);
+	if (text[startOffset] && text[startOffset + 1] !== "/") {
 		tagRE.lastIndex = startOffset;
-		const m = text.match(tagRE);
-		endOffset = tagRE.lastIndex;
+		const 
+			m = text.match(tagRE),
+			endOffset = tagRE.lastIndex;
 		if (m && offset <= tagRE.lastIndex) {
 			const 
 				range = new vsc.Range(doc.positionAt(startOffset), doc.positionAt(endOffset)),
@@ -55,6 +33,37 @@ export default function getTagMatch (doc: vsc.TextDocument, pos: vsc.Position) {
 				attribDetails = attribArr.map(v => v.match(attrRE)),
 				spaceAfterName = m.groups?.C || "",
 				wrapped = !!spaceAfterName.match("\n");
+			const
+				tagStr = m[0],
+				mm = tagStr.match(styleRE);
+			let styleOpts = {};
+				if (mm) {
+					const
+						styleAttrStr = mm[0],
+						styleStartOffset = startOffset + (mm.index || 0),
+						attrLength = styleAttrStr.length,
+						styleEndOffset = styleStartOffset + attrLength,
+						range = new vsc.Range(
+							doc.positionAt(styleStartOffset), 
+							doc.positionAt(styleEndOffset)
+						),
+						quoted = mm.groups?.quoted;
+					styleOpts = {
+						range
+					};
+					if (quoted) {
+						const 
+							quoteType = quoted[0],
+							contentStr = quoted.slice(1, -1);
+						
+						styleOpts = {
+							...styleOpts,
+							quoteType,
+							contentStr,
+						};
+					}
+						
+				}
 			
 			return {
 				match: m,
@@ -81,4 +90,34 @@ export default function getTagMatch (doc: vsc.TextDocument, pos: vsc.Position) {
 		// vsc.window.showWarningMessage("No begin");
 	}
 	return null;
+}
+
+function getBaseIndent(text: string, offset: number) {
+	let i = offset, v = "", baseIndent = "";
+	while (v = text[i]) {
+		if (v === "\n") {
+			break;
+		}
+		i --;
+	}
+	i += 1;
+	while (v = text[i]) {
+		if (![" ", "\t"].includes(v)) {
+			break;
+		}
+		baseIndent += v;
+		i ++;
+	}
+	return baseIndent;
+}
+
+function getTagStartOffset(text: string, givenOffset: number) {
+	let i = givenOffset, v = "";
+	while (v = text[i]) {
+		if (v === "<") {
+			return i;
+		}
+		i --;
+	}
+	return -1;
 }
