67c71ef2bd93ff39b769432c6f19ad2583b27ea9
Bognaum <bognaum@gmail.com>
2022-06-21 10:39:32 +0300

    Improved.

--- src/functions/getTagMatch.ts
+++ src/functions/getTagMatch.ts
@@ -12,79 +12,81 @@ const
 
 export default function getTagMatch (doc: vsc.TextDocument, pos: vsc.Position) {
 	const
-		offset = doc.offsetAt(pos),
+		givenOffset = doc.offsetAt(pos),
 		text = doc.getText();
-	const 
-		startOffset = getTagStartOffset(text, offset),
-		baseIndent  = getBaseIndent(text, startOffset);
-	if (text[startOffset] && text[startOffset + 1] !== "/") {
+
+	const startOffset = getTagStartOffset(text, givenOffset);
+	if (-1 < startOffset) {
 		tagRE.lastIndex = startOffset;
-		const 
-			m = text.match(tagRE),
-			endOffset = tagRE.lastIndex;
-		if (m && offset <= tagRE.lastIndex) {
-			const 
-				range = new vsc.Range(doc.positionAt(startOffset), doc.positionAt(endOffset)),
-				openingBr = m.groups?.A || "",
-				tagName = m.groups?.tagName || "",
-				attribStr = m.groups?.attribs || "",
-				closingBr = m.groups?.B || "",
-				attribArr = attribStr.match(attributesRE)?.map(v => v.trim()) || [],
-				attribDetails = attribArr.map(v => v.match(attrRE)),
-				spaceAfterName = m.groups?.C || "",
-				wrapped = !!spaceAfterName.match("\n");
-			const
-				tagStr = m[0],
-				mm = tagStr.match(styleRE);
-			let styleOpts = {};
-				if (mm) {
-					const
-						styleAttrStr = mm[0],
-						styleStartOffset = startOffset + (mm.index || 0),
-						attrLength = styleAttrStr.length,
-						styleEndOffset = styleStartOffset + attrLength,
-						range = new vsc.Range(
-							doc.positionAt(styleStartOffset), 
-							doc.positionAt(styleEndOffset)
-						),
-						quoted = mm.groups?.quoted;
-					styleOpts = {
-						range
-					};
-					if (quoted) {
-						const 
-							quoteType = quoted[0],
-							contentStr = quoted.slice(1, -1);
-						
+		const m = text.match(tagRE);
+		if (m) {
+			const endOffset = tagRE.lastIndex;
+			if (startOffset <= givenOffset && givenOffset <= tagRE.lastIndex) {
+				const 
+					baseIndent  = getBaseIndent(text, startOffset),
+					range = new vsc.Range(doc.positionAt(startOffset), doc.positionAt(endOffset)),
+					openingBr = m.groups?.A || "",
+					tagName = m.groups?.tagName || "",
+					attribStr = m.groups?.attribs || "",
+					closingBr = m.groups?.B || "",
+					attribArr = attribStr.match(attributesRE)?.map(v => v.trim()) || [],
+					attribDetails = attribArr.map(v => v.match(attrRE)),
+					spaceAfterName = m.groups?.C || "",
+					wrapped = !!spaceAfterName.match("\n");
+				const
+					tagStr = m[0],
+					mm = tagStr.match(styleRE);
+				let styleOpts = {};
+					if (mm) {
+						const
+							styleAttrStr = mm[0],
+							styleStartOffset = startOffset + (mm.index || 0),
+							attrLength = styleAttrStr.length,
+							styleEndOffset = styleStartOffset + attrLength,
+							range = new vsc.Range(
+								doc.positionAt(styleStartOffset), 
+								doc.positionAt(styleEndOffset)
+							),
+							quoted = mm.groups?.quoted;
 						styleOpts = {
-							...styleOpts,
-							quoteType,
-							contentStr,
+							range
 						};
+						if (quoted) {
+							const 
+								quoteType = quoted[0],
+								contentStr = quoted.slice(1, -1);
+							
+							styleOpts = {
+								...styleOpts,
+								quoteType,
+								contentStr,
+							};
+						}
+							
 					}
-						
-				}
-			
-			return {
-				match: m,
-				range,
-				startOffset,
-				endOffset,
-				tagStr: m[0],
-				"<": openingBr, 
-				tagName, 
-				attribStr, 
-				attribArr,
-				attribDetails,
-				attribCount: attribArr.length,
-				">": closingBr,
-				givenDoc: doc,
-				givenPos: pos,
-				givenOffset: offset,
-				baseIndent,
-				wrapped,
-				spaceAfterName,
-			};
+				
+				return {
+					match: m,
+					range,
+					startOffset,
+					endOffset,
+					tagStr: m[0],
+					"<": openingBr, 
+					tagName, 
+					attribStr, 
+					attribArr,
+					attribDetails,
+					attribCount: attribArr.length,
+					">": closingBr,
+					givenDoc: doc,
+					givenPos: pos,
+					givenOffset,
+					baseIndent,
+					wrapped,
+					spaceAfterName,
+				};
+			}
+
 		}
 	} else {
 		// vsc.window.showWarningMessage("No begin");
